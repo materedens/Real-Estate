0d722f911fbeccb7ccbcfad0f9dccf2e1035c17c
Denis Matere <denismatere@gmail.com>
2022-09-06 15:31:08 +0300

    about update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import './about.css';
+import back from '../images/Background.png';
 
 const About = () => {
     return (
         <>
-            <h1>About Us</h1>
+            {/* Breadcrumb area */}
+            <section className='breadcrumb_area'>
+                <div className='overlay bg-parallax'>
+
+                </div>
+                <div className='container'>
+                    <div className='page-cover text-center'>
+                        <h2 className='page-cover-title'>About Us</h2>
+                        <ol className='breadcrumb'>
+                            <li><a href='#home'>Home</a> </li>
+                            <li className='active'>About Us</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+            {/* About area */}
+            <section className='about section_gap'>
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col-md-6 d-flex align-items-center'>
+                            <div className='about_content'>
+                                <h2 className='title'>About Us</h2>
+                                <p> We construct both residential estates and offices.
+                                    We construct both residential estates and offices.
+                                    We construct both residential estates and offices.
+                                    We construct both residential estates and offices.
+                                </p>
+                            </div>
+                        </div>
+                        <div className='col-md-5'>
+                            <img className='img-fluid' src={back} alt='Cool Place' />
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import Form from 'react-bootstrap';
-
+// import Form from 'react-bootstrap';
+import { Grid, Paper, TextField, Button, TextareaAutosize } from '@mui/material';
 const Contact = () => {
+    const paperStyle = { padding: '30px 20px', width: 1300, margin: "50px auto", backgroundColor: '#fff' }
+    const textfieldStyle = { margin: '5px', padding: '5px' }
+    const buttonStyle = { padding: '10px', margin: '10px', backgroundColor: '#f03c02' }
+    const textareaStyle = { width: 1250, margin: 10, padding: 5, backgroundColor: '#fff' }
+
     return (
         <>
             <div className='breadcrumbs d-flex align-items-center'>
@@ -10,12 +15,18 @@ const Contact = () => {
                 </div>
 
             </div>
-            <Form>
-                <Form.FormGroup className='mb-3' controlId='name'>
-                    <Form.Label>Name:</Form.Label>
-                    <Form.Control type='text' placeholder='Your Name' />
-                </Form.FormGroup>
-            </Form>
+            <Grid>
+                <Paper elevation={20} style={paperStyle}>
+                    <form>
+                        <TextField label='Your Name' placeholder='Your Name' fullWidth required style={textfieldStyle} />
+                        <TextField label='Your Email' placeholder='Your Email' fullWidth required style={textfieldStyle} />
+                        <TextField label='Subject' placeholder='Subject' fullWidth required style={textfieldStyle} />
+                        <TextareaAutosize label='Message' placeholder='Message' minRows={5} maxRows={5} style={textareaStyle} required fullWidth />
+                        <Button type='submit' variant='contained' color='primary' style={buttonStyle}>Send Message</Button>
+                    </form>
+                </Paper>
+
+            </Grid>
         </>
     )
 }
