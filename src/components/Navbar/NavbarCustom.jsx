39ba5a5f9052a8f13f1a5bf894fd1117126eaff5
Denis Matere <denismatere@gmail.com>
2022-09-07 18:17:11 +0300

    navbarupdate

--- src/components/Navbar/NavbarCustom.jsx
+++ src/components/Navbar/NavbarCustom.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
-import './navbar.css';
+// import { NavLink } from 'react-router-dom';
+// import { LinkContainer } from 'react-router-dom';
+// import './navbar.css';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const NavbarCustom = () => {
     return (
         <>
-            <Navbar default collapseOnSelect bg="light" variant="light" className='navbar'>
+            <Navbar collapseOnSelect expand='sm' bg="light" variant="light" sticky='top'>
                 <Container>
-                    <Navbar.Brand path='/'>
-                        Cool Place
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Nav>
-                        <NavLink to="/" >Home</NavLink>
-                        <NavLink to="/about">About</NavLink>
-                        <NavLink to="/services">Services</NavLink>
-                        <NavLink to="/contact">Contact</NavLink>
-                    </Nav>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            Cool Place
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+                        <Nav className='mr-auto my-2 my-lg-0'
+                            style={{ maxHeight: '100' }} navbarScroll >
+                            <LinkContainer to="/">
+                                <Nav.Link>Home</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/about">
+                                <Nav.Link>About</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/services">
+                                <Nav.Link>Services</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/contact">
+                                <Nav.Link>Contact</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
